5cd76ce41d38c9aa9635a7e313eb06cee0f35c29
RAR2025 <ruturaj.rajwade@gmail.com>
2026-06-19 17:21:37 +0530

    prepared admin routes

--- backend/src/controller/admin.controller.js
+++ backend/src/controller/admin.controller.js
@@ -1,15 +1,30 @@
 import {Song} from "../models/song.model.js"
 import {Album} from "../models/album.model.js"
+import cloudinary from "../lib/cloudinary.js"
+
+const uploadToCloundinary = async (file) => {
+    try {
+        const result = await cloudinary.upload(file.tempFilePath, {
+            resource_type: "auto",
+
+        })
+        return result.secure_url
+    } catch (error) {
+        console.log(`Error in uploadToCloudinary`, error);
+        throw new Error ("error uploading to cloudinary");
+    }
+}
 export const createSong = async (req, res, next) => {
     
     try {
         if(!req.files || !req.files.audioFile || !req.files.imageFile){
             return res.status(400).json({message: "Please upload all files "})
         }    
-
         const {title, artist, albumId, duration} = req.body;
         const audioFile = req.files.audioFile 
         const imageFile = req.files.imageFile
+        const audioUrl = await uploadToCloudinary(audioFile)
+        const imageUrl = await uploadToCloudinary(imageFile)
         
         const song = new Song({
             title,
@@ -36,4 +51,73 @@ export const createSong = async (req, res, next) => {
         // res.status(500).json({message: "Internal server error", error})
         next(error)
     }
+};
+
+export const deleteSong = async (req, res, next) => {
+    try {
+        const {id } = req.params;
+        const song = await Song.findById(id);
+        //if song belongs ot album then update that song
+        if(song.albumId){
+            $pull : {songs:song_id}
+
+        }
+
+        await Song.findByIdAndDelete(id);
+        res.status(200).json({message: "Song deleted successfully"})        
+    } catch (error) {
+        console.log(`error in deleteSong`, error);
+        next(error)
+    }
 }
+
+export const createAlbum= async (req, res, next) => {
+    
+    try {
+        if(!req.files  || !req.files.imageFile){
+            return res.status(400).json({message: "Please upload all files "})
+        }    
+        
+        const {title, artist, releaseYear} = req.body; 
+        const imageFile = req.files.imageFile
+        const imageUrl = await uploadToCloudinary(imageFile)
+        
+        const album = new Album({
+            title,
+            artist,
+            imageUrl,
+            releaseYear,
+        });
+
+
+        await album.save();
+
+
+        
+
+        res.status(201).json(album);
+    } catch (error) {
+        console.log(`error in createAlbum`, error);
+        // res.status(500).json({message: "Internal server error", error})
+        next(error)
+    }
+};
+
+
+export const deleteAblum = async (req, res, next) => {
+    try {
+        const {id} = req.params;
+        const album = await Album.findById(id);
+
+        await Album.findByIdAndDelete(id);
+        res.status(200).json({message: "Album deleted successfully"})   
+
+    }catch (error) {
+        console.log(`error in deleteSong`, error);
+        next(error)
+    }
+}
+
+export const checkAdmin = async (req, res, next) => {
+	res.status(200).json({ admin: true });
+};
