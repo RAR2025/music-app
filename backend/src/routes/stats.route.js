434c1966b88a8f9824335f32772085d69c88de80
RAR2025 <ruturaj.rajwade@gmail.com>
2026-06-20 22:32:55 +0530

    partial stats route  design

--- backend/src/routes/stats.route.js
+++ backend/src/routes/stats.route.js
@@ -1,7 +1,42 @@
 import { Router } from "express";
+import {Song} from '../models/song.model.js'
+import {User} from '../models/user.model.js'
+import {Album} from '../models/album.model.js'
 const router = Router()
 
-router.get('/' , (req, res) => {
-    res.send(`stats route via get method`)
+router.get('/' , async (req, res, next) => {
+    try {
+        
+        const [totalSongs, totalUsers, totalAlbums, usiqueArtists] = await Promise.all([
+            Song.countDocuments(),
+            User.countDocuments(),
+            Album.countDocuments(),
+            Song.aggregate([
+                {
+                    $uniqueWith: {
+                        coll: 'albums',
+                        pipeline: []
+                    }
+                },
+                {
+                    $group: {
+                        _id: "$artist",
+                    }
+                },
+                {
+                    $count: "count",
+                },
+            ])
+        ]);
+
+        res.json(200).json({
+            totalSongs,
+            totalUsers,
+            totalAlbums,
+            usiqueArtists: usiqueArtists[0]?.count || 0,
+        });
+    } catch (error) {
+        next(error);
+    }
 })
 export default router
